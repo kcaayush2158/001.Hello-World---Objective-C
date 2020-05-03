32b9bf0ba061933d78954bddf105ce8997f32764
Aayush K.C <aayushkc2158@gmail.com>
2020-05-03 23:38:23 +0545

    hello world in obj c

---
+++ 002.Interface/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  002.Interface
+//
+//  Created by Aayush Kc on 5/3/20.
+//  Copyright © 2020 Aayush Kc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+NSLog(@"Hello, World!");
+    return 0;
+
+}
